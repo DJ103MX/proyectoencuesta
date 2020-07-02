8727c23b8057a8fcbc388d48fedfacdebd0982ae
DJ103MX <51246750+DJ103MX@users.noreply.github.com>
2020-07-01 23:48:41 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
+define('DB_SERVER', 'q57yawiwmnaw13d2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('DB_USERNAME', 'b89j1waxhxwrcsi6');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'demo');
+define('DB_NAME', 'co8wp6olsaxilld3');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
